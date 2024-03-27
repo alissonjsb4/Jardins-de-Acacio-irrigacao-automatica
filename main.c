f76fc8f6e969c84642675ac13751b3c3c04a9cfe
Gabriel Amorim <137211057+gabrielmelo7@users.noreply.github.com>
2024-03-27 12:03:13 -0300

    Add files via upload
    Código da STM com funções do clock e de irrigação

---
+++ main.c
@@ -0,0 +1,389 @@
+/* USER CODE BEGIN Header */
+	/**
+	  ******************************************************************************
+	  * @file           : main.c
+	  * @brief          : Main program body
+	  ******************************************************************************
+	  * @attention
+	  *
+	  * Copyright (c) 2024 STMicroelectronics.
+	  * All rights reserved.
+	  *
+	  * This software is licensed under terms that can be found in the LICENSE file
+	  * in the root directory of this software component.
+	  * If no LICENSE file comes with this software, it is provided AS-IS.
+	  *
+	  ******************************************************************************
+	  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+RTC_HandleTypeDef hrtc;
+HAL_RTC_AlarmTypeDef dAlarm;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_RTC_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+
+
+/* USER CODE BEGIN 0 */
+
+void init_rtc(){
+	RTC_TimeTypeDef time;
+	time.Hours = 12;
+	time.Minutes = 0;
+	time.Seconds = 0;
+
+	RTC_DateTypeDef date;
+	date.Year = 2024;
+	date.Month = 1;
+	date.WeekDay = 1;
+
+	HAL_RTC_SetDate(&hrtc, &date, RTC_FORMAT_BIN);
+	HAL_RTC_SetTime(&hrtc, &time, RTC_FORMAT_BIN);
+}
+
+void set_rtc(uint8_t hour, uint8_t minute, uint8_t second, uint8_t year, uint8_t month, uint8_t day){
+	RTC_TimeTypeDef time;
+	time.Hours = hour;
+	time.Minutes = minute;
+	time.Seconds = second;
+
+	RTC_DateTypeDef date;
+	date.Year = year;
+	date.Month = month;
+	date.WeekDay = day;
+
+	HAL_RTC_SetDate(&hrtc, &date, RTC_FORMAT_BIN);
+	HAL_RTC_SetTime(&hrtc, &time, RTC_FORMAT_BIN);
+}
+
+void set_alarm(uint8_t hour, uint8_t minute, uint8_t second, uint8_t year, uint8_t day){
+		RTC_TimeTypeDef time;
+		time.Hours = hour;
+		time.Minutes = minute;
+		time.Seconds = second;
+
+		RTC_DateTypeDef date;
+		date.Year = year;
+		date.Month = month;
+		date.WeekDay = day;
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+		bool irrigando;
+		bool ir_progamada;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  init_rtc();
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_RTC_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	  while (1)
+	  {
+
+		  char c = '0'; //RECEBE A INSTRUÇÃO DA ESP
+
+
+		  GPIO_PinState pin_state = HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_11);
+
+
+
+		  switch(c){
+
+			  case 'i': //REALIZA IRRIGAÇÃO INSTANTÂNEA, PRECISA DO TEMPO DE DURAÇÃO DA IRRIGAÇÃO
+				  if(!irrigando){
+					//Inicia a irrigação
+					 irrigando = true;
+					 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET);
+				  }
+				  else{
+					  //Passar pra esp que já está ligado
+				  }
+				  //FUNÇÃO DE RETORNAR Q DEU TUDO CERTO PRA ESP
+				  break;
+
+			  case 'c': //CESSA A IRRIGAÇÃO ATUAL
+				  if(irrigando){
+					  irrigando = false;
+					  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET);
+					  break;
+				  else{
+					  //Passar pra esp que já está desligado
+				  }
+				  break;
+
+			  case 'a': //AGENDA IRRIGAÇÃO DIÁRIA
+				  if(){
+
+				  }
+
+				  break;
+
+			  case 'd': //DESATIVA A IRRIGAÇÃO DIÁRIA
+				  if(!ir_progamada){
+					  //RETORNAR A ESP QUE JA ESTÁ DESATIVADA
+					  break;
+				  }
+				  //IMPLEMENTAR FUNÇÃO QUE DESATIVA A IRRIGAÇÃO
+				  ir_progamada = 0;
+				  break;
+
+			  case 's':
+				  //Passar o "irrigando" pra esp
+				  break;
+
+			  case m: //MODIFICA O HORÁRIO DA STM
+				  //Pega hora, minuto, segundo, ano, mês e dia da esp
+				  if(set_rtc(hour, minute, second, year, month, day) != HAL_OK){
+					  Error_Handler();
+				  }
+
+		  }
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief RTC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_RTC_Init(void)
+{
+
+  /* USER CODE BEGIN RTC_Init 0 */
+
+  /* USER CODE END RTC_Init 0 */
+
+  RTC_TimeTypeDef sTime = {0};
+  RTC_DateTypeDef sDate = {0};
+
+  /* USER CODE BEGIN RTC_Init 1 */
+
+  /* USER CODE END RTC_Init 1 */
+
+  /** Initialize RTC Only
+  */
+  hrtc.Instance = RTC;
+  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+  hrtc.Init.AsynchPrediv = 127;
+  hrtc.Init.SynchPrediv = 255;
+  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+  if (HAL_RTC_Init(&hrtc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN Check_RTC_BKUP */
+
+  /* USER CODE END Check_RTC_BKUP */
+
+  /** Initialize RTC and set the Time and Date
+  */
+  sTime.Hours = 0x0;
+  sTime.Minutes = 0x0;
+  sTime.Seconds = 0x0;
+  sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+  sTime.StoreOperation = RTC_STOREOPERATION_RESET;
+  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BCD) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sDate.WeekDay = RTC_WEEKDAY_MONDAY;
+  sDate.Month = RTC_MONTH_JANUARY;
+  sDate.Date = 0x1;
+  sDate.Year = 0x0;
+
+  if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BCD) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN RTC_Init 2 */
+
+  /* USER CODE END RTC_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB6 PB7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+	  /* User can add his own implementation to report the HAL error return state */
+	  __disable_irq();
+	  while (1)
+	  {
+	  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+	  /* User can add his own implementation to report the file name and line number,
+		 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
